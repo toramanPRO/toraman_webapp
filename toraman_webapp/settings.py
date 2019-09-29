e492596858cf75537c0a629fa762371ab9b21d4a
cagatay <cagataysengor@gmail.com>
2019-09-29 23:40:06 +0300

    version 0.0.3

--- toraman_webapp/settings.py
+++ toraman_webapp/settings.py
@@ -15,7 +15,7 @@ import random
 
 from .local_settings import *
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 VERSION = __version__
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
