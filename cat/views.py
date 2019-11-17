eb3ba1b28f2ff146c1463726351c57dd4daf5938
cagatay <cagataysengor@gmail.com>
2019-11-17 21:19:56 +0300

    fixed a bug where the status of empty segments crashed the editor view

--- cat/views.py
+++ cat/views.py
@@ -94,7 +94,10 @@ def bilingual_file(request, user_id, project_id, source_file):
             for segment in paragraph:
                 source_segment = segment_to_html(segment[0])
                 target_segment = segment_to_html(segment[2])
-                segment_status = segment[1].text.lower()
+                if segment[1].text is not None:
+                    segment_status = segment[1].text.lower()
+                else:
+                    segment_status = ''
                 paragraph_no = segment[3]
                 segment_no = segment[4]
 
