12891893052aecdba9b24ced1bb98f8fd5084af8
cagatay <cagataysengor@gmail.com>
2019-12-15 10:31:18 +0300

    removed redundant lines

--- toraman_webapp/urls.py
+++ toraman_webapp/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cat/', include('cat.urls')),
