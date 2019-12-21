a8ef81139b4c3ef8846be46035dbee37a31caae2
cagatay <cagataysengor@gmail.com>
2019-12-21 11:10:49 +0300

    added new parameters to local_settings_template.py

--- toraman_webapp/local_settings_template.py
+++ toraman_webapp/local_settings_template.py
@@ -2,11 +2,14 @@
 DEBUG = False
 
 # The first item on ALLOWED_HOSTS list should show your domain.
-# ALLOWED_HOSTS = ['example.com', '127.0.0.1']
+#ALLOWED_HOSTS = ['example.com', '127.0.0.1']
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# import random, string
-# SECRET_KEY = ''.join(random.choice(string.ascii_letters + string.punctuation + string.digits) for i in range(64))
+# SECRET_KEY typically consists of 64 random letters, punctuations and digits.
+# The short snippet below generates the required code.
+#   import random, string
+#   ''.join(random.choice(string.ascii_letters + string.punctuation + string.digits) for i in range(64))
+#SECRET_KEY =
 
 # E-mail settings
 #EMAIL_BACKEND =
@@ -15,7 +18,13 @@ DEBUG = False
 #EMAIL_HOST_USER =
 #EMAIL_HOST_PASSWORD =
 #EMAIL_USE_TLS =
+# Default e-mail address for e-mails sent to users
 #DEFAULT_FROM_EMAIL =
+# Default e-mail address for e-mails sent to ADMINS/MANAGERS (may be set to DEFAULT_FROM_EMAIL)
+#SERVER_EMAIL =
+
+# List of admin(s) to receive details of exceptions by e-mail
+#ADMINS = [('name', 'email'),]
 
 # Default user permissions
 #USERS_CAN_ADD_PROJECTS_BY_DEFAULT = True
